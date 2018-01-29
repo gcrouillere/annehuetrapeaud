88edece195817310d2982db8a3f62d6dc752b8c9
guillaume crouillere <gcrouillere@hotmail.fr>
2018-01-29 12:45:14 +0100

    ok

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -1,14 +1,14 @@
 module MetaTagsHelper
   def meta_title
-    content_for?(:meta_title) ? content_for(:meta_title) : "Les #{ENV['MODEL']} de #{ENV['FIRSTNAME'].capitalize} #{ENV['LASTNAME'].capitalize}"
+    content_for?(:meta_title) ? content_for(:meta_title) : "Les #{ENV['MODEL']} de #{ENV['FIRSTNAME'].capitalize} Huet Rapeaud"
   end
 
   def meta_product_name
-    content_for?(:meta_product_name) ? content_for(:meta_product_name) : "Les #{ENV['MODEL']} de #{ENV['FIRSTNAME'].capitalize} #{ENV['LASTNAME'].capitalize} - vente de produits de l'artisanat"
+    content_for?(:meta_product_name) ? content_for(:meta_product_name) : "Les #{ENV['MODEL']} de #{ENV['FIRSTNAME'].capitalize} Huet Rapeaud - vente de produits de l'artisanat"
   end
 
   def meta_description
-    description = "Des produits extraordinaires. Décor somptueux. Venez découvrir leur fabrication lors d'un stage."
+    description = "Des céramiques somptueuses. Venez découvrir leur fabrication lors d'un stage."
     content_for?(:meta_description) ? content_for(:meta_description) : description
   end
 
