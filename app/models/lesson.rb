3e59dce4b90aa915e5845cf771a7a9f38f40928f
guillaume crouillere <gcrouillere@gmail.com>
2020-11-25 21:39:29 +0100

    fix new lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,7 +3,7 @@ class Lesson < ApplicationRecord
 
   belongs_to :user
   belongs_to :calendarupdate
-  has_one :order
+  has_one :order, dependent: :destroy
 
   validates :start, presence: true
   validates :duration, presence: true, numericality: { only_integer: true, greater_than: 0 }
